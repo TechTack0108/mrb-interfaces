8c0de464218a7500b8202821d599b0e8e264b73a
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2022-01-01 07:04:04 +0700

    updated mrb interface

--- folder.interface.ts
+++ folder.interface.ts
@@ -1,9 +1,10 @@
 export default interface IFolder {
-  _id?: string;
-  name: string;
-  parentFolderID?: string | null;
-  shareFolderID?: string | null;
-  path_display?: string | null;
-  isRootFolder?: boolean;
-  numFiles: number;
+	_id?: string;
+	name: string;
+	parentFolderID?: string | null;
+	shareFolderID?: string | null;
+	path_display?: string | null;
+	isRootFolder?: boolean;
+	numFiles: number;
+	updatedAt: string;
 }
