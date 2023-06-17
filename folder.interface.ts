fc923ea2428f919077d532cb7b25c81bf6a9103b
LamT0305 <lam45883@gmail.com>
2023-06-17 15:53:12 +0700

    update user interface

--- folder.interface.ts
+++ folder.interface.ts
@@ -2,4 +2,7 @@ export default interface IFolder {
   _id?: string;
   name: string;
   parentFolderID?: string | null;
+  shareFolderID?: string | null;
+  path_display?: string | null;
+  isRootFolder?: boolean;
 }
