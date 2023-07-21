7361dc7c31d7725763629c6e452cf39b03b8e593
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-07-21 15:28:41 +0700

    some changes to timeline interface

--- timeline.interface.ts
+++ timeline.interface.ts
@@ -4,12 +4,21 @@ import IDocument from "./document.interface";
  * Interface for each year of the timeline.
  * @example
  * {"2019": [
- *   {"Thang 1": [IDocument, IDocument, ...]},
- *   {"Thang 2": [IDocument, IDocument, ...]},
+ *   {"Thang 1": [
+ *     {"Ngay 1": [IDocument, IDocument, ...]},
+ *     {"Ngay 2": [IDocument, IDocument, ...]},
+ *     ...
+ *     {"Ngay 31": [IDocument, IDocument, ...]}
+ *   ]},
  *   ...
- *   {"Thang 12": [IDocument, IDocument, ...]}
+ *  {"Thang 12": [
+ *     {"Ngay 1": [IDocument, IDocument, ...]},
+ *     {"Ngay 2": [IDocument, IDocument, ...]},
+ *     ...
+ *     {"Ngay 31": [IDocument, IDocument, ...]}
+ *   ]},
  * ]}
  * */
 export interface ITimeline {
-	[key: string]: { [key: string]: IDocument[] }[];
+	[year: string]: { [month: string]: { [day: string]: IDocument[] }[] }[];
 }
