4207a7a1406f5d18f2d0cdb572d349cd51f05639
Tin Du Vo <55805983+t-op2019@users.noreply.github.com>
2023-08-27 15:23:25 +0700

    update document interface

--- document.interface.ts
+++ document.interface.ts
@@ -24,3 +24,24 @@ export default interface IDocument {
 	classificationID: string;
 	categoryID: string;
 }
+
+export enum DocumentFieldMongo {
+	FIELD_ID = "$fieldId",
+	SUBJECT = "$subject",
+	REF_NO = "$refNo",
+	PRE_REF_NO = "$preRefNo",
+	SENDER = "$sender",
+	RECEIVER = "$receiver",
+	FOLDER_ID = "$folderId",
+	FILE_PATH = "$filePath",
+	RECEIVE_DATE = "$receiveDate",
+	UPLOAD_DATE = "$uploadDate",
+	FILE_NAME = "$fileName",
+	FORMATTED_NAME = "$formattedName",
+	RENAMED_FILE_NAME = "$renamedFileName",
+	FILE_TYPE = "$fileType",
+	DOCUMENT_TEXT = "$documentText",
+	DOCUMENT_SIZE = "$documentSize",
+	CLASSIFICATION_ID = "$classificationID",
+	CATEGORY_ID = "$categoryID",
+}
